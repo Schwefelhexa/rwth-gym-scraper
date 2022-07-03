d833f0cd91cf947b7eda947dbf8f6ed628584949
Alexander Baron <alexander.baron1@rwth-aachen.de>
2022-07-03 20:27:06 +0200

    feat: Recognize image to text

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
+import Tesseract from "tesseract.js";
+import sharp from "sharp";
+
 import { fetch_image } from "./fetch_image";
 
 (async () => {
-  const path = await fetch_image();
-  console.log(`Saved image to ${path}`);
+  const imagePath = await fetch_image();
+
+  const imagePngPath = imagePath.replace(".gif", ".png");
+  await sharp(imagePath).toFile(imagePngPath);
+
+  const {
+    data: { text },
+  } = await Tesseract.recognize(imagePngPath, "eng");
+  console.log(`I think the current occupancy is ${text}`);
 })();
